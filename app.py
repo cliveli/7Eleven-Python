ef00792e23e3c0f96031591b11fa738b1e4f8fc4
cliveli <mfkwmfk@gmail.com>
2020-05-30 23:15:34 +1000

    proxy

--- app.py
+++ app.py
@@ -20,7 +20,7 @@
 
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug import DispatcherMiddleware
 from werkzeug.serving import run_simple
 # Optional Security
 # Uncomment Basic Auth section to enable basic authentication so users will be prompted a username and password before seeing the website.
